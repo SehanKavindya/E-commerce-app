32b68d865d7aba8cecc46fa09a1718d4c2106f13
SehanKavindya <21ict053@seu.ac.lk>
2026-01-23 09:31:37 +0530

    Footer Creation Edit

--- src/components/organisms/Footer.jsx
+++ src/components/organisms/Footer.jsx
@@ -2,7 +2,7 @@ import { Container, Row ,Col } from "react-bootstrap";
 
 const Footer = () => {
 return (
-    <footer className="bg-dark text-light py">
+    <footer className="bg-dark text-light py-3 mt-auto">
         <Container>
             <Row>
                <Col className="text-center">
